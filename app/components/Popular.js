f005ef62bb59b523ad8a861c231ba41039280035
Emily Zhang <qinglu.zh@gmail.com>
2019-08-11 23:29:13 -0400

    Click language in nav bar to change state and update element color

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,11 +1,26 @@
 import React from 'react'
 
 export default class Popular extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      selectedLanguage: 'All'
+    }
+
+    this.updateLanguage = this.updateLanguage.bind(this);
+  }
+
+  updateLanguage(selectedLanguage) {
+    this.setState({
+      selectedLanguage
+    });
+  }
   render() {
     const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
 
     return (
-      //*
+      /*
       // use React.createElement()
       React.createElement(
         'ul',
@@ -16,7 +31,11 @@ export default class Popular extends React.Component {
             { key: index },
             React.createElement(
               'button',
-              { className: 'btn-clear nav-link' },
+              {
+                className: 'btn-clear nav-link',
+                onClick: () => this.updateLanguage(language),
+                style: language === this.state.selectedLanguage ? { color: 'red' } : null
+              },
               language
             )
           )
@@ -24,17 +43,23 @@ export default class Popular extends React.Component {
       )
       /**/
 
-      /* 
+      //* 
       // use JSX
-      <ul>
+      <ul className='flex-center'>
         {
-          languages.map((language, index) => (
-            <li key={ index }>
-              <button className='btn-clear nav-link'>
-                { language }
+          languages.map((language, index) => {
+            return (
+            <li key={index}>
+              <button 
+                className='btn-clear nav-link' 
+                style={language === this.state.selectedLanguage ? { color: 'red' } : null} 
+                onClick={() => this.updateLanguage(language)}
+              >
+                {language}
               </button>
             </li>
-          ))
+            );
+          })
         }
       </ul>
       /* */
