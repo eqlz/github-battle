4d656b9fb00482426a5ab7f24308adfec4d34d78
Emily Zhang <qinglu.zh@gmail.com>
2019-08-18 21:42:11 -0400

    Implement api to get user data for battle result

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,3 +1,74 @@
+function getUserData(playerUserName) {
+  return Promise.all([
+    getProfile(playerUserName),
+    getRepos(playerUserName)
+  ])
+  .then(([profile, repos]) => ({
+    profile,
+    score: calculateScore(profile.followers, repos)
+  }))
+}
+
+function getProfile(userName) {
+  return fetch(`https://api.github.com/users/${userName}`)
+    .then((response) => {
+      return response.json();
+    })
+    .then((profile) => {
+      if(profile.message) {
+        throw new Error(
+          profile.message === 'Not Found'
+            ? `${userName} doesn't exist`
+            : profile.message
+        );
+      }
+      return profile;
+    })
+}
+
+function getRepos(userName) {
+  return fetch(`https://api.github.com/users/${username}/repos&per_page=100`)
+    .then((response) => {
+      response.json();
+    })
+    .then((repos) => {
+      if(repos.message) {
+        throw new Error(
+          repos.message === 'Not Found'
+            ? `${userName} doesn't exist`
+            : repos.message
+        );
+      }
+      return repos;
+    })
+}
+
+function calculateScore(numOfFollowers, repos) {
+  return (numOfFollowers * 3) + getStarCount(repos);
+}
+
+function getStarCount(repos) {
+  return repos.reduce((totoalStarCount, repo) => {
+    return totoalStarCount + repo.stargazers_count;
+  }, 0)
+}
+
+function sortPlayers(players) {
+  return players.sort((a, b) => {
+    return b.score - a.score;
+  })
+}
+
+export function battle(playerUserNames) {
+  return Promise.all([
+    getUserData(playerUserNames[0]),
+    getUserData(playerUserNames[1])
+  ])
+  .then((results) => {
+    sortPlayers(results)
+  })
+}
+
 export function fetchPopularRepos (language) {
   const endPoint = window.encodeURI(`https://api.github.com/search/repositories?q=stars:>1+language:
     ${language}&sort=stars&order=desc&type=Repositories`);
