fd736d71906c0c69b03c6d00d52e2cfb0c3bc656
Emily Zhang <qinglu.zh@gmail.com>
2019-08-19 21:57:44 -0400

    Populate Results component state with battle result

--- app/components/Results.js
+++ app/components/Results.js
@@ -2,18 +2,39 @@ import React from 'react'
 import { getBattleResults } from '../utils/api'
 
 export default class Results extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      winner: null,
+      loser: null,
+      error: null,
+      loading: true,
+    }
+  }
   componentDidMount() {
     const { playerOneUserName, playerTwoUserName } = this.props;
     getBattleResults([playerOneUserName, playerTwoUserName])
       .then((battleResults) => {
-        console.log('battle results: ', battleResults);
+        this.setState({
+          winner: battleResults[0],
+          loser: battleResults[1],
+          error: null,
+          loading: false,
+        })
+      })
+      .catch(({ message }) => {
+        this.setState({
+          error: message,
+          loading: false,
+        })
       })
   }
   render() {
     return (
       <div>
         Results
-        <pre>{JSON.stringify(this.props, null, 2)}</pre>
+        <pre>{JSON.stringify(this.state, null, 2)}</pre>
       </div>
     )
   }
