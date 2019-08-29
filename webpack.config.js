4793fd68790f1971de17417549d14247e1098e50
Emily Zhang <qinglu.zh@gmail.com>
2019-08-28 21:26:36 -0400

    Add extension reolve to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,21 +2,22 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './app/index.js',
+  entry: './app/index.jsx',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'index_bundle.js'
+    filename: 'index_bundle.js',
   },
+  resolve: { extensions: ['.jsx', '.js', '.json'] },
   module: {
     rules: [
-      { test: /\.(js)$/, use: 'babel-loader' },
-      { test: /\.css$/, use: ['style-loader', 'css-loader'] }
-    ]
+      { test: /\.(js|jsx)$/, use: 'babel-loader' },
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+    ],
   },
   mode: 'development',
   plugins: [
     new HtmlWebpackPlugin({
-      template: './app/index.html'
-    })
-  ]
-}
+      template: './app/index.html',
+    }),
+  ],
+};
