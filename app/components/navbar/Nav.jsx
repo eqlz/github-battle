a771e7b6e961d948d4e420d58f509e49a8785718
Emily Zhang <qinglu.zh@gmail.com>
2019-09-04 21:38:46 -0400

    Add navigation link to popular and battle in nav bar

--- app/components/navbar/Nav.jsx
+++ app/components/navbar/Nav.jsx
@@ -1,11 +1,38 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+
 import ThemeContext from '../../contexts/theme';
+
+const activeStyle = {
+  color: 'red',
+};
 
 function Nav() {
   return (
     <ThemeContext.Consumer>
       {({ theme, toggleTheme }) => (
         <nav className="row space-between">
+          <ul className="row nav">
+            <li>
+              <NavLink
+                exact
+                to="/"
+                activeStyle={activeStyle}
+                className="nav-link"
+              >
+                Popular
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/battle"
+                activeStyle={activeStyle}
+                className="nav-link"
+              >
+                Battle
+              </NavLink>
+            </li>
+          </ul>
           <button
             type="button"
             style={{ fontSize: 30 }}
