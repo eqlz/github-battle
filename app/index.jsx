774b1929be89bd69c6f81d6b5f92af2b07bb3858
Emily Zhang <qinglu.zh@gmail.com>
2019-09-11 20:51:51 -0400

    Implement lazy loading

--- app/index.jsx
+++ app/index.jsx
@@ -6,12 +6,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './index.css';
+import Loading from './components/shared/Loading';
 
-import Popular from './components/popular/Popular';
-import Battle from './components/battle/Battle';
-import Results from './components/battle/Results';
 import ThemeContext from './contexts/theme';
 import Nav from './components/navbar/Nav';
+
+const Popular = React.lazy(() => import('./components/popular/Popular'));
+const Battle = React.lazy(() => import('./components/battle/Battle'));
+const Results = React.lazy(() => import('./components/battle/Results'));
 
 class App extends React.Component {
   constructor(props) {
@@ -34,12 +36,14 @@ class App extends React.Component {
           <div className={this.state.theme}>
             <div className="container">
               <Nav />
-              <Switch>
-                <Route exact path="/" component={Popular} />
-                <Route exact path="/battle" component={Battle} />
-                <Route path="/battle/results" component={Results} />
-                <Route render={() => <h1>Page Not Found</h1>} />
-              </Switch>
+              <React.Suspense fallback={<Loading />}>
+                <Switch>
+                  <Route exact path="/" component={Popular} />
+                  <Route exact path="/battle" component={Battle} />
+                  <Route path="/battle/results" component={Results} />
+                  <Route render={() => <h1>Page Not Found</h1>} />
+                </Switch>
+              </React.Suspense>
             </div>
           </div>
         </ThemeContext.Provider>
