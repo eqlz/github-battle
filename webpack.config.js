b2f064eb7a7a6ccc24e84d29df0d84bf812bb132
Emily Zhang <qinglu.zh@gmail.com>
2019-09-16 20:53:07 -0400

    Set to production mode directly

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
       { test: /\.css$/, use: ['style-loader', 'css-loader'] },
     ],
   },
-  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+  mode: 'production',
   plugins: [
     new HtmlWebpackPlugin({
       template: './app/index.html',
